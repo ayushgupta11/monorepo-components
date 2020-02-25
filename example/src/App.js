7fc75700aa6f05c828ba33fcbc3b923e9b6cb019
ayush_1197 <ayush.gupta.1197@gmail.com>
2020-02-25 11:42:58 +0530

    Base structure setup

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'lumenore-monorepo'
+import { ExampleComponent } from 'lumenore-monorepo'
 
 export default class App extends Component {
   render () {
